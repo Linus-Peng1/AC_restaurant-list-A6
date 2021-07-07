36c671d4544f54b0c092ac57436f74b1bf0b283c
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-07-07 22:29:06 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 
 const port = 3000
 const app = express()
 
+// 資料庫連線設定
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
@@ -18,9 +20,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定路由
 app.get('/', (req, res) => {
-  res.send('Restaurant List')
+  res.render('index')
 })
 
 // 設定 port 3000
