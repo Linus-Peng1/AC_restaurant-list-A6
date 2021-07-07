2f7a986fb5638f950636491f6157edf25769d497
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-07-07 21:54:47 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 const port = 3000
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定路由
 app.get('/', (req, res) => {
