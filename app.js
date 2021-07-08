3ac930eb1793d30ecdcdddd27a95cab878883b48
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-07-08 21:29:22 +0800

    feat: add edit restaurant function

--- app.js
+++ app.js
@@ -53,6 +53,34 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.error(error))
 })
 
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, name_en, category, location, phone, rating, google_map, image, description } = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
 app.get('/restaurants/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
   if (keyword === "") {
