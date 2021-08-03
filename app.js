7f2aefe72db24e48c204ec47bad48a13396fb716
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-08-03 15:12:19 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ require('./config/mongoose')
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+// 設定本地變數 res.locals，所有的 view 都可以存取
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 // 設定路由
 app.use(routes)
 
